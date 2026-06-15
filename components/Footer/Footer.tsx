50e7a545eb2078a17945b758a7383cdd4e93cc61
Yuliia <asee198814@gmail.com>
2026-06-15 18:41:51 +0200

    add hm-06

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
           <p>Developer: your name</p>
           <p>
            Contact us:
-           <Link href="<mailto:student@notehub.app>">student@notehub.app</Link>
+           <Link href="mailto:student@notehub.app">student@notehub.app</Link>
           </p>
         </div>
       </div>
